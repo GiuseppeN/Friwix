ea8fbd769e80fd1eca55401a4c9743da44a51bb1
Giuseppe Nugara <g.nugara@themadbox.com>
2016-06-14 17:40:40 +0200

    fix

--- Friwix/Friwix.h
+++ Friwix/Friwix.h
@@ -15,5 +15,5 @@ FOUNDATION_EXPORT double FriwixVersionNumber;
 FOUNDATION_EXPORT const unsigned char FriwixVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Friwix/PublicHeader.h>
-
+#import <Friwix/test.h>
 
