3ed1c5ad3eac5f486255845e36f8d6d502907ab2
Giuseppe Nugara <g.nugara@themadbox.com>
2016-06-14 17:26:31 +0200

    fix

---
+++ Friwix/Test.h
@@ -0,0 +1,17 @@
+//
+//  Test.h
+//  Friwix
+//
+//  Created by TheMadBox on 14/06/16.
+//  Copyright © 2016 TheMadBox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@property (nonatomic, strong) NSString *name;
+
+- (instancetype)initWithName:(NSString *) name;
+
+@end
---
+++ Friwix/Test.m
@@ -0,0 +1,23 @@
+//
+//  Test.m
+//  Friwix
+//
+//  Created by TheMadBox on 14/06/16.
+//  Copyright © 2016 TheMadBox. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+- (instancetype)initWithName:(NSString *) name
+{
+    self = [super init];
+    if (self) {
+        self.name = name;
+    }
+    return self;
+}
+
+
+@end
